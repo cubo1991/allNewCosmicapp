58462261bf4964479cd8b2da48006c64b0d97456
cubo1991 <adavidlopezmathez@gmail.com>
2023-12-26 13:46:49 -0300

    Fixes I

--- src/app/components/joinMatch.js
+++ src/app/components/joinMatch.js
@@ -1,13 +1,30 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 
 export const JoinMatch = ({ value, onChange }) => {
     
     const { register, handleSubmit, setValue } = useForm();
+    let user;
+    if (typeof window !== 'undefined') {
+      user = JSON.parse(localStorage.getItem("user"));
+    }
+    const [partida, setPartida] = useState(user ? 'idPartida' in user : false);
 
     useEffect(() => {
       setValue('id', value);
+      if (user && 'idPartida' in user) { 
+        setPartida(true)
+      }
     }, [value, setValue]);
+
+    const eraseGame = () => {
+      if (typeof window !== 'undefined') {
+        let userToModify =  JSON.parse(localStorage.getItem('user'))
+        delete userToModify.idPartida
+        localStorage.setItem('user', JSON.stringify(userToModify))
+        setPartida(false);
+      }
+    }
 
     const onSubmit = (data) => {
       console.log(data.id)
@@ -17,10 +34,18 @@ export const JoinMatch = ({ value, onChange }) => {
     return (
       <div>
         <h2>Unirse a una partida</h2>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input {...register('id')} />
-          <input type="submit" />
-        </form>
+        {partida ? (
+          <div>
+            <p>Ya tienes una partida iniciada. ¿Quieres borrarla?</p>
+            <button onClick={eraseGame}>Sí</button>
+            <button>No</button>
+          </div>
+        ) : (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input {...register('id')} />
+            <input type="submit" />
+          </form>
+        )}
       </div>
     )
   }
