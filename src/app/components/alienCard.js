4acb51c15aa802eb12dbe13510a85df304c1e4b4
cubo1991 <adavidlopezmathez@gmail.com>
2024-01-01 21:53:41 -0300

    Nuevo año 4

--- src/app/components/alienCard.js
+++ src/app/components/alienCard.js
@@ -18,21 +18,23 @@ export const AlienCard = ({key, alien}) => {
     <div className="border border-gray-200 rounded-lg p-4 shadow-lg bg-white">
       {alien && (
         <table className="w-full">
-          <tr>
-            <td className="w-2/3">
-              <h2 className=" mt-2 text-2xl font-bold">{alien.Nombre}</h2>
-            </td>
-            <td className="w-1/3 flex justify-end items-center">
-              <span className={`h-4 w-4 rounded-full ${difficultyColor[alien.Dificultad]}`}></span>
-            </td>
-          </tr>
-          <tr>
-            <td colSpan="2">
-            <h3 className="font-bold text-lg">{alien.Poder}</h3>
-              <p className={`overflow-hidden ${isExpanded ? '' : 'max-h-14'}`}>{alien.Descripción? alien.Descripción : 'Chupala, no me voy a poner a llenar manualmente este poder'}</p>
-              <button onClick={handleExpandClick} className="text-blue-500 mt-2">{isExpanded ? 'Ver menos' : 'Ver más'}</button>
-            </td>
-          </tr>
+          <tbody>
+            <tr>
+              <td className="w-2/3">
+                <h2 className=" mt-2 text-2xl font-bold">{alien.Nombre}</h2>
+              </td>
+              <td className="w-1/3 flex justify-end items-center">
+                <span className={`h-4 w-4 rounded-full ${difficultyColor[alien.Dificultad]}`}></span>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="2">
+              <h3 className="font-bold text-lg">{alien.Poder}</h3>
+                <p className={`overflow-hidden ${isExpanded ? '' : 'max-h-14'}`}>{alien.Descripción? alien.Descripción : 'Chupala, no me voy a poner a llenar manualmente este poder'}</p>
+                <button onClick={handleExpandClick} className="text-blue-500 mt-2">{isExpanded ? 'Ver menos' : 'Ver más'}</button>
+              </td>
+            </tr>
+          </tbody>
         </table>
       )}
     </div>
